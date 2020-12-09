f6df690e56c5d36eb9453f4d3411846a8cc4cc47
8 <lucinda.souchet@outlook.com>
2020-12-09 16:04:17 +0000

    modify contact app and gallery app relative to form + css

--- contact/forms.py
+++ contact/forms.py
@@ -9,3 +9,27 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'email', 'message')
+
+
+def __init__(self, *args, **kwargs):
+    """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+    """
+    super().__init__(*args, **kwargs)
+    # placeholders = {
+    #     'first_name': 'First name',
+    #     'last_name': 'Last name',
+    #     'email': 'Email',
+    #     'message': 'Message',
+    # }
+    #
+    # self.fields['first_name'].widget.attrs['autofocus'] = True
+    # for field in self.fields:
+    #     if self.fields[field].required:
+    #         placeholder = f'{placeholders[field]} *'
+    #     else:
+    #         placeholder = placeholders[field]
+    #     self.fields[field].widget.attrs['placeholder'] = placeholder
+    #     self.fields[field].widget.attrs['class'] = 'contact-style-input'
+    #     self.fields[field].label = False
