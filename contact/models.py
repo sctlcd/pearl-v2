2c75bd57620c818aa933e0f70015ce82c82ffb78
8 <lucinda.souchet@outlook.com>
2020-12-09 23:36:02 +0000

    review checkout, contact and gallery models

--- contact/models.py
+++ contact/models.py
@@ -4,10 +4,11 @@ from django.db import models
 # Contact models
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50, null=False, blank=False)
-    last_name = models.CharField(max_length=50, null=False, blank=False)
+    first_name = models.CharField(max_length=254, null=False, blank=False)
+    last_name = models.CharField(max_length=254, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    message = models.TextField()
+    message = models.TextField(null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
