84bd9344b782dcf9e09c4e77363a790cbfcd7a9f
8 <lucinda.souchet@outlook.com>
2020-12-11 22:39:53 +0000

    add Edit product template, url and view

--- products/views.py
+++ products/views.py
@@ -97,3 +97,29 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """
+        Edit a product in the store
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        product_form = ProductForm(request.POST, request.FILES, instance=product)
+        if product_form.is_valid():
+            product_form.save()
+            messages.success(request, 'Succeed to edit product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the product form is valid.')
+    else:
+        product_form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'product_form': product_form,
+        'product': product,
+    }
+
+    return render(request, template, context)
