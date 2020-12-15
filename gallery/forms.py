7fc6ea7bd22b91d0f909901045368d97e0ddebd6
8 <lucinda.souchet@outlook.com>
2020-12-15 16:49:43 +0000

    add custom_file_input.html, widgets.py, update GalleryForm form

--- gallery/forms.py
+++ gallery/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Gallery, GalleryCategory
 
 
@@ -11,13 +12,19 @@ class GalleryForm(forms.ModelForm):
         fields = ('user_name', 'email', 'author_name',
                   'gallery_category', 'image', 'note',)
 
+    image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
 
-def __init__(self, *args, **kwargs):
-    """
-        Set autofocus on first field
-    """
-    super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """
+            Set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
 
-    self.fields['user_name'].widget.attrs['autofocus'] = True
-    for field in self.fields:
-        self.fields[field].widget.attrs['class'] = 'border-grey'
+        gallerycategories = GalleryCategory.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in gallerycategories]
+
+        self.fields['gallery_category'].choices = friendly_names
+
+        self.fields['user_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'border-grey'
