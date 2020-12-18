479456f274e9f084fa58592b4a834b80933d15a8
8 <lucinda.souchet@outlook.com>
2020-12-18 01:26:59 +0000

    add admin edit gallery functionality, update image field display and custom_clearable_file_input widget

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('share/', views.share_gallery, name='share_gallery'),
     path('add/', views.add_gallery, name='add_gallery'),
+    path('edit/<gallery_id>/', views.edit_gallery, name='edit_gallery'),
 ]
