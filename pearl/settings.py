0e30be3c3415b4a9a6336a684903518f1ec2dcda
8 <lucinda.souchet@outlook.com>
2020-12-22 14:28:20 +0000

    set up ALLOWED_HOSTS, DATABASE_URL in settings.py

--- pearl/settings.py
+++ pearl/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '!673$ex3ca^-5$+9xi21a*b%g&kw$rae8x-kfk!e$$2q*pjyl9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sctlcd-pearl.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,12 +124,17 @@ WSGI_APPLICATION = 'pearl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
