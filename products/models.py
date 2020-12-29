c8fad2e8653d85b5cfef1aa26171ad42395b4ea4
8 <lucinda.souchet@outlook.com>
2020-12-29 10:45:29 +0000

    product forms: add placeholders and classes

--- products/models.py
+++ products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
-    price = models.DecimalField(max_digits=6, decimal_places=2,  default=0, null=True, blank=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2,  default=0, null=False, blank=False)
     rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True, default=0, validators=[MaxValueValidator(5), MinValueValidator(0)])
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
