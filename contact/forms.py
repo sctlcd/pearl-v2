225f25c738c5df726ed2a897f6b113b7301da21c
8 <lucinda.souchet@outlook.com>
2020-12-29 12:18:24 +0000

    update allauth templates and style

--- contact/forms.py
+++ contact/forms.py
@@ -10,17 +10,24 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        """
+            Add placeholders and classes, set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'message': 'Message',
+        }
 
-def __init__(self, *args, **kwargs):
-    """
-        Set autofocus on first field
-    """
-    super().__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['autofocus'] = True
 
-    self.fields['first_name'].widget.attrs['autofocus'] = True
-
-    for field in self.fields:
-        self.fields[field].widget.attrs['class'] = 'border-grey text-grey rounded-10'
-
-    # for field_name, field in self.fields.items():
-    #     field.widget.attrs['class'] = 'border-grey text-grey rounded-10'
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-grey rounded-10'
--- contact/views.py
+++ contact/views.py
@@ -15,11 +15,10 @@ def contact(request):
 
         if contact_form.is_valid():
             contact_form.save()
-            messages.success(request, 'Your request has been sent successfully!')
-            return redirect(reverse('home'))
+            messages.success(request, 'Your contact request has been sent successfully!')
+            return redirect(reverse('contact'))
         else:
-            # contact_form = ContactForm()
-            messages.error(request, 'Your request has failed. Please ensure the contact form is valid.')
+            messages.error(request, 'Your contact request has failed. Please ensure the contact form is valid.')
     else:
         contact_form = ContactForm()
 
